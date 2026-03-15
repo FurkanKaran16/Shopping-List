4066e8b938225c0d881d8a74eb30652012d703fe
Furkan Karan <furkankaranc@gmail.com>
2026-03-15 18:24:48 +0300

    add remove item

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const shoppingList = document.querySelector(".shopping-list");
 const shoppingForm = document.querySelector(".shopping-form");
 
-loadItems();
-shoppingForm.addEventListener("submit", handleFormSubmit);
+document.addEventListener("DOMContentLoaded",function() {
+    loadItems();
+    shoppingForm.addEventListener("submit", handleFormSubmit);
+});
 
 function loadItems() {
     const items = [
@@ -51,12 +53,19 @@ function handleFormSubmit(e) {
     addItem(input);
 }
 
+function toggleCompleted(e){
+    const li = e.target.parentElement;
+    li.toggleAttribute("item-completed",e.target.checked);
+}
+
+
 function createListItem(item) {
     // checkbox
     const input = document.createElement("input");
     input.type = "checkbox";
     input.classList.add("form-check-input");
     input.checked = item.completed;
+    input.addEventListener("change",toggleCompleted);
 
     // item
     const div = document.createElement("div");
@@ -64,16 +73,22 @@ function createListItem(item) {
     div.classList.add("item-name");
 
     // delete icon
-    const deleteIcon = document.createElement("span");
+    const deleteIcon = document.createElement("i");
     deleteIcon.className = "fs-3 bi bi-x text-danger delete-icon";
+    deleteIcon.addEventListener("click",removeItem);
 
     // li
     const li = document.createElement("li");
-    li.className = "border rounded p-3 mb-1";
+    li.className = "border rounded p-2 mb-1";
+    li.toggleAttribute("item-completed",item.completed);
 
     li.appendChild(input);
     li.appendChild(div);
     li.appendChild(deleteIcon);
 
     return li;
+}
+function removeItem(e) {
+    const li = e.target.parentElement;
+    shoppingList.removeChild(li);
 }
