9544db5523960ffa50d26e3325e8a08190e698cd
Furkan Karan <furkankaranc@gmail.com>
2026-03-15 18:50:11 +0300

    add edit item

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const shoppingList = document.querySelector(".shopping-list");
 const shoppingForm = document.querySelector(".shopping-form");
 
-document.addEventListener("DOMContentLoaded",function() {
+document.addEventListener("DOMContentLoaded", function() {
     loadItems();
     shoppingForm.addEventListener("submit", handleFormSubmit);
 });
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded",function() {
 function loadItems() {
     const items = [
         {id:1, name: "Yumurta", completed: false },
-        {id:2, name: "Balik", completed: true },
+        {id:2, name: "Balık", completed: true },
         {id:3, name: "Süt", completed: false },
         {id:4, name: "Zeytin", completed: false },
     ];
@@ -53,11 +53,10 @@ function handleFormSubmit(e) {
     addItem(input);
 }
 
-function toggleCompleted(e){
+function toggleCompleted(e) {
     const li = e.target.parentElement;
-    li.toggleAttribute("item-completed",e.target.checked);
+    li.toggleAttribute("item-completed", e.target.checked);
 }
-
 
 function createListItem(item) {
     // checkbox
@@ -65,22 +64,25 @@ function createListItem(item) {
     input.type = "checkbox";
     input.classList.add("form-check-input");
     input.checked = item.completed;
-    input.addEventListener("change",toggleCompleted);
+    input.addEventListener("change", toggleCompleted);
 
     // item
     const div = document.createElement("div");
     div.textContent = item.name;
     div.classList.add("item-name");
+    div.addEventListener("click", openEditMode);
+    div.addEventListener("blur", closeEditMode);
+    div.addEventListener("keydown", cancelEnter);
 
     // delete icon
     const deleteIcon = document.createElement("i");
     deleteIcon.className = "fs-3 bi bi-x text-danger delete-icon";
-    deleteIcon.addEventListener("click",removeItem);
+    deleteIcon.addEventListener("click", removeItem);
 
     // li
     const li = document.createElement("li");
     li.className = "border rounded p-2 mb-1";
-    li.toggleAttribute("item-completed",item.completed);
+    li.toggleAttribute("item-completed", item.completed);
 
     li.appendChild(input);
     li.appendChild(div);
@@ -88,7 +90,26 @@ function createListItem(item) {
 
     return li;
 }
+
 function removeItem(e) {
     const li = e.target.parentElement;
     shoppingList.removeChild(li);
+}
+
+function openEditMode(e) {
+    const li = e.target.parentElement;
+    if(li.hasAttribute("item-completed") == false) {
+        e.target.contentEditable = true;
+    }
+}
+
+function closeEditMode(e) {
+    e.target.contentEditable = false;
+}
+
+function cancelEnter(e) {
+    if(e.key == "Enter") {
+        e.preventDefault();
+        closeEditMode(e);
+    }
 }
